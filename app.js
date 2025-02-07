3cfab502bf47021ab8ce5f80632b4e0a686f3f72
Victor Arriola <vicdarrsa@gmail.com>
2025-02-06 20:44:29 -0600

    Se agrega funcion agregaAmigo

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Lista de amigos
 let listaDeAmigos = [];
+
+
+function agregarAmigo() {
+  let nombreDelAmigo = document.getElementById("amigo").value;
+  if (nombreDelAmigo == "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    listaDeAmigos.push(nombreDelAmigo);
+    document.getElementById("amigo").value = "";
+  }
+  console.log(listaDeAmigos);
+}
